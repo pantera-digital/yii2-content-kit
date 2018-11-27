83d8e65d767279d8b813c398718f1ae1ddb398cd
serega <r.s.i@bk.ru>
2018-11-27 13:22:16 +1000

    fix

--- widgets/slider/views/index.php
+++ widgets/slider/views/index.php
@@ -17,35 +17,45 @@ use yii\web\View;
     <?php foreach ($models as $model) : ?>
         <div class="item content-slider__item">
             <?php
+                if ($model->url) {
+                    echo Html::beginTag('a', [
+                        'href' => $model->url,
+                    ]);
+                }
+            ?>
+            <?php
             $img = '';
             if ($model->media && $model->media->issetMedia()) {
                 $img = Html::img($model->media->getUrl());
             }
-            if ($model->url) {
-                echo Html::a($img, $model->url);
-            } else {
-                echo $img;
-            }
+            echo $img;
             ?>
-            <div class="content-slider__item-content">
-                <?php
-                if ($model->row_1) {
-                    echo Html::tag('div', $model->row_1, [
-                        'class' => 'content-slider__item-row content-slider__item-row--1',
-                    ]);
+            <?php if ($model->row_1 || $model->row_2 || $model->row_3) : ?>
+                <div class="content-slider__item-content">
+                    <?php
+                    if ($model->row_1) {
+                        echo Html::tag('div', $model->row_1, [
+                            'class' => 'content-slider__item-row content-slider__item-row--1',
+                        ]);
+                    }
+                    if ($model->row_2) {
+                        echo Html::tag('div', $model->row_2, [
+                            'class' => 'content-slider__item-row content-slider__item-row--2',
+                        ]);
+                    }
+                    if ($model->row_3) {
+                        echo Html::tag('div', $model->row_3, [
+                            'class' => 'content-slider__item-row content-slider__item-row--3',
+                        ]);
+                    }
+                    ?>
+                </div>
+            <?php endif; ?>
+            <?php
+                if ($model->url) {
+                    echo Html::endTag('a');
                 }
-                if ($model->row_2) {
-                    echo Html::tag('div', $model->row_2, [
-                        'class' => 'content-slider__item-row content-slider__item-row--2',
-                    ]);
-                }
-                if ($model->row_3) {
-                    echo Html::tag('div', $model->row_3, [
-                        'class' => 'content-slider__item-row content-slider__item-row--3',
-                    ]);
-                }
-                ?>
-            </div>
+            ?>
         </div>
     <?php endforeach; ?>
 </div>
