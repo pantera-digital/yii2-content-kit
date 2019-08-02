f2aefee3661e20df9017ebc1f8d0a8e0753d2e13
Alex Kostin <web-mayak@mail.ru>
2019-08-03 01:15:06 +1000

    доработка getMenuItems: фикс для выключенного prettyUrl + активация пунктов

--- admin/Module.php
+++ admin/Module.php
@@ -57,14 +57,22 @@ class Module extends \yii\base\Module
 
     public function getMenuItems()
     {
+        $currentModule = Yii::$app->controller->module->id == 'content';
+        $controllerId = Yii::$app->controller->id;
         $items = [
-            ['label' => Yii::t('content', 'Type'), 'url' => ['/content/type/index']],
-            ['label' => Yii::t('content', 'Slider'), 'url' => ['/content/slider/index']],
-            ['label' => Yii::t('content', 'Block'), 'url' => ['/content/block/index']],
+            ['label' => Yii::t('content', 'Type'), 'url' => ['/content/type/index'], 'active' => $currentModule && $controllerId == 'type'],
+            ['label' => Yii::t('content', 'Slider'), 'url' => ['/content/slider/index'], 'active' => $currentModule && $controllerId == 'slider'],
+            ['label' => Yii::t('content', 'Block'), 'url' => ['/content/block/index'], 'active' => $currentModule && $controllerId == 'block'],
         ];
         $types = ContentType::find()->all();
         foreach ($types as $type) {
-            $items[] = ['label' => $type->name, 'url' => ['/content/page/' . $type->key]];
+            $items[] = [
+                'label' => $type->name,
+                'url' => ['/content/page/index', 'key' => $type->key],
+                'active' => $currentModule
+                         && $controllerId == 'page'
+                         && Yii::$app->request->get('key') == $type->key,
+            ];
         }
         return [['label' => Yii::t('content', 'Content'), 'url' => '#', 'icon' => 'file-text', 'items' => $items]];
     }
