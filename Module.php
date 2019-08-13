38d3178dba48c378882c233f86d5e7c5b94f13b4
Alex Kostin <web-mayak@mail.ru>
2019-08-13 22:58:01 +1000

    добавил useBlock, useSlider, useMedia, useSeo в frontend-модуль

--- Module.php
+++ Module.php
@@ -11,4 +11,13 @@ namespace pantera\content;
 class Module extends \yii\base\Module
 {
     const SLUG_FRONT_PAGE = '<front>';
+    
+    /* @var bool Управление блоками */
+    public $useBlock = true;
+    /* @var bool Управление слайдами */
+    public $useSlider = true;
+    /* @var bool Использовать pantera-digital/yii2-media для страниц */
+    public $useMedia = true;
+    /* @var bool Использовать pantera-digital/yii2-seo для страниц */
+    public $useSeo = true;
 }
